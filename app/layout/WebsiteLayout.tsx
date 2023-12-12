b54cb47e8ec26f948c3ec473aa06cd826474df02
qiaoyinbo <breakerjoe@163.com>
2023-12-12 08:25:53 +0800

    add card component

--- app/layout/WebsiteLayout.tsx
+++ app/layout/WebsiteLayout.tsx
@@ -1,5 +1,49 @@
-export default function Website() {
-    return (
-        <div>Website works!</div>
-    )
+import {
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  Divider,
+  Link,
+  Image,
+} from "@nextui-org/react";
+import React from "react";
+
+export default function Website({ data }) {
+  let websites = data.websites;
+  return (
+    <div className="mt-20 grid grid-cols-4 gap-4 mx-4">
+      {websites.map((item, index) => (
+        <Card className="max-w-[400px]" key={index}>
+          <CardHeader className="flex gap-3">
+            <Image
+              alt="nextui logo"
+              height={40}
+              radius="sm"
+              src="https://avatars.githubusercontent.com/u/86160567?s=200&v=4"
+              width={40}
+            />
+            <div className="flex flex-col">
+              <p className="text-md">{item.name}</p>
+              <p className="text-small text-default-500">{item.url}</p>
+            </div>
+          </CardHeader>
+          <Divider />
+          <CardBody>
+            <p>{item.description}</p>
+          </CardBody>
+          <Divider />
+          <CardFooter>
+            <Link
+              isExternal
+              showAnchorIcon
+              href={item.url}
+            >
+              Visit website.
+            </Link>
+          </CardFooter>
+        </Card>
+      ))}
+    </div>
+  );
 }
--- pages/website.tsx
+++ pages/website.tsx
@@ -1,18 +1,24 @@
 import Website from "app/layout/WebsiteLayout";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+import fs from 'fs';
+import path from "path";
 
-export default function WebsitePage() {
+export default function WebsitePage({ data }) {
   return (
     <main>
-      <Website />
+      <Website data={data}/>
     </main>
   );
 }
 
 export async function getStaticProps({ locale }) {
+  const filePath = path.join(process.cwd(), 'public/data', 'website.json')
+  const file = await fs.readFileSync(filePath, 'utf-8');
+  const data = JSON.parse(file);
   return {
     props: {
       ...(await serverSideTranslations(locale, ["common"])),
+      data,
     },
   };
 }
